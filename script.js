eb3ea4573ae0dc37effb21bbb8cc348fe0e94bd3
KB Desktop <Keithblack4290gmail.com>
2021-01-24 15:11:23 -0700

    pushing to make sure BCS has udpdated repo. Line 47 error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // once I start the quiz the timer will start to countdown for 75 sec
 var timeEl = document.querySelector("#time-left");
 var startBtn = document.getElementById("button");
-var secondsLeft = 75;
+var secondsLeft = 76;
 var quizTile = document.querySelector("#title");
 var questionCounter = 0;
 var results = document.querySelector("#result");
@@ -41,22 +41,17 @@ var questions = [{
 
 ]
 
-
-
-
 // When I click a choice then it will go to the next question
 function nextQuestion() {
     document.getElementById("choice").innerHTML = "";
     document.getElementById("title").innerHTML = questions[questionCounter].questions;
+    console.log(questions[questionCounter].questions);
+
 
     // create for loop to cycle through each choice to create new button element
     for (var i = 0; i < questions[questionCounter].choices.length; i++) {
         var choiceBtn = document.createElement("BUTTON")
         choiceBtn.textContent = questions[questionCounter].choices[i];
-
-
-
-
 
         choiceBtn.addEventListener("click", () => {
             correctChoice();
@@ -93,32 +88,49 @@ function correctChoice() {
     }
     questionCounter++;
     nextQuestion();
-
-}
+    }
 
 
 // the start button will start the time and go to question 1
 startBtn.addEventListener("click", () => {
     startTime();
     nextQuestion();
+    hideStartQuiz();
 
 });
 
+// This function hides the intro and the questions will appear
+
+function hideStartQuiz() {
+
+    var hideStart = document.getElementById("start-quiz");
+
+    if (hideStart.style.display === "none") {
+        hideStart.style.display = "block";
+    }
+    else {
+        hideStart.style.display = "none";
+    }
+}
+
+// hiding all done div until all questions have been answered. then hide questions div
+document.getElementById("all-done").style.display = "none";
 
 
 // timer function starts the time count down by 1 
 function startTime() {
     console.log("startTime");
     var timerInterval = setInterval(function () {
-
-        timeEl.textContent = secondsLeft;
         secondsLeft--;
+        timeEl.textContent = secondsLeft;
 
         if (secondsLeft === 0 || questionCounter === 5) {
-            clearInterval(timerInterval); // this is where timer needs to reach 0
-
-
-        }
+            clearInterval(timerInterval);
+            // when time reaches 0 or all 5 questions are answered then the quiz questions are hidden and all done page is shown
+            document.getElementById("quiz-question").style.display = "none";
+            document.getElementById("all-done").style.display = "block"; 
+             // this is where timer needs to reach 0
+            }
     }, 1000);
 }
 
